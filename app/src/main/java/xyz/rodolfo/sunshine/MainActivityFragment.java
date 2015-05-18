823baf1008e0fe9810255e1fd3db01ef75e3cf3e
Rodolfo Stangherlin <rodolfo.3@gmail.com>
2015-05-18 01:12:09 -0300

    List data from (mock) code.

--- app/src/main/java/xyz/rodolfo/sunshine/MainActivityFragment.java
+++ app/src/main/java/xyz/rodolfo/sunshine/MainActivityFragment.java
@@ -5,6 +5,13 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 
 /**
@@ -18,6 +25,29 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
-    }
+
+        String[] forecastArray = new String[] {
+            "A",
+            "B",
+            "C"
+        };
+
+        List<String> weekForecast = new ArrayList<>(
+                Arrays.asList(forecastArray)
+        );
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(
+                this.getActivity(),
+                R.layout.list_item_forecast,
+                R.id.list_item_forecast_textview,
+                weekForecast
+        );
+
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+        ListView listView = (ListView) rootView.findViewById(R.id.listview_forecast);
+        listView.setAdapter(adapter);
+
+        return rootView;
+                    }
 }
